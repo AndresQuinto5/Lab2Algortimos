65a37a58ee8678800e63280e1c2d556a13ad4aa3
AndresQuinto5 <42017522+AndresQuinto5@users.noreply.github.com>
2019-01-31 17:50:03 -0600

    Add files via upload

---
+++ src/iCalculadora.java
@@ -0,0 +1,7 @@
+//AndresQuinto-18288
+public interface iCalculadora {
+    public double sumar(double num1, double num2);
+    public double restar(double num1, double num2);
+    public double multiplicacion(double num1, double num2);
+    public double division(double num1, double num2);
+}
